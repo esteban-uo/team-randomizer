82a19d7f755c016e544f023718ea4be830dd8df3
Esteban Uscanga <esteban@liquidlabs.de>
2016-06-03 11:17:32 +0200

    int

---
+++ randomnizer.js
@@ -0,0 +1,18 @@
+
+function onSubmit() {
+    var names = $('#names').prop('value').split(',').map( (name) => name.trim());
+
+    console.log(names);
+}
+
+$('#submit').click(onSubmit);
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
